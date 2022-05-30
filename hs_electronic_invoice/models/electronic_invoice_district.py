2ed09acf8c60ce6ba2ae3a970f5e76048f103be9
ERILEX <edelgado@hermecsolutions.com>
2022-05-30 10:52:41 -0500

    translate

--- hs_electronic_invoice/models/electronic_invoice_district.py
+++ hs_electronic_invoice/models/electronic_invoice_district.py
@@ -2,10 +2,10 @@ from odoo import models, fields, api
 class electronic_invoice_district(models.Model):
 	_name = 'electronic.invoice.district'
 
-	code = fields.Char(string='Código', size=3, required=True)
+	code = fields.Char(string='Código', size=3, required=True, translate=True)
 	name = fields.Char(string='Nombre', size=255, required=True, translate=True)
-	country_id = fields.Many2one('res.country', string='País', required=False, compute='_get_country_id', store=True)
-	province_id = fields.Many2one('electronic.invoice.province', string='Provincia', required=False)
+	country_id = fields.Many2one('res.country', string='País', required=False, translate=True, compute='_get_country_id', store=True)
+	province_id = fields.Many2one('electronic.invoice.province', string='Provincia', required=False, translate=True)
 	sector_ids = fields.One2many('electronic.invoice.sector', 'district_id', string='Corregimientos')
 
 	@api.depends('name')
--- hs_electronic_invoice/models/electronic_invoice_sector.py
+++ hs_electronic_invoice/models/electronic_invoice_sector.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api
 class electronic_invoice_sector(models.Model):
 	_name = 'electronic.invoice.sector'
-	code = fields.Char(string='Código', size=3, required=True)
+	code = fields.Char(string='Código', size=3, required=True, translate=True)
 	name = fields.Char(string='Nombre', size=255, required=True, translate=True)
-	country_id = fields.Many2one('res.country', string='País', required=False, compute='_get_country_id', store=True)
-	province_id = fields.Many2one('electronic.invoice.province', string='Provincia', required=False)
-	district_id = fields.Many2one('electronic.invoice.district', string='Distrito', required=False)
+	country_id = fields.Many2one('res.country', string='País', required=False, translate=True, compute='_get_country_id', store=True)
+	province_id = fields.Many2one('electronic.invoice.province', string='Provincia', required=False, translate=True)
+	district_id = fields.Many2one('electronic.invoice.district', string='Distrito', required=False, translate=True)
 
 	@api.depends('name')
 	def _get_country_id(self):
