be8959c70e68938988306f3b3d5faf68d8856227
ERILEX <edelgado@hermecsolutions.com>
2022-05-30 10:14:19 -0500

    remove translate -cascade

--- hs_electronic_invoice/models/electronic_invoice_sector.py
+++ hs_electronic_invoice/models/electronic_invoice_sector.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api
 class electronic_invoice_sector(models.Model):
 	_name = 'electronic.invoice.sector'
-	code = fields.Char(string='Código', size=3, required=True, translate=True)
+	code = fields.Char(string='Código', size=3, required=True)
 	name = fields.Char(string='Nombre', size=255, required=True, translate=True)
-	country_id = fields.Many2one('res.country', string='País', required=False, translate=True, compute='_get_country_id', store=True)
-	province_id = fields.Many2one('electronic.invoice.province', string='Provincia', required=False, translate=True)
-	district_id = fields.Many2one('electronic.invoice.district', string='Distrito', required=False, translate=True)
+	country_id = fields.Many2one('res.country', string='País', required=False, compute='_get_country_id', store=True)
+	province_id = fields.Many2one('electronic.invoice.province', string='Provincia', required=False)
+	district_id = fields.Many2one('electronic.invoice.district', string='Distrito', required=False)
 
 	@api.depends('name')
 	def _get_country_id(self):
